59b676af2288566bba59e01c48abe76a303111bd
Dyako Baram <55919885+dyako-baram@users.noreply.github.com>
2021-03-12 19:10:01 +0300

    impleamenting update functunality

--- WpfWithAutoUpdate/MainWindow.xaml.cs
+++ WpfWithAutoUpdate/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Squirrel;
 
 namespace WpfWithAutoUpdate
 {
@@ -20,6 +21,7 @@ namespace WpfWithAutoUpdate
     /// </summary>
     public partial class MainWindow : Window
     {
+        UpdateManager update;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,11 +30,28 @@ namespace WpfWithAutoUpdate
         {
             
         }
-        private void mainWindow_Loaded(object sender, RoutedEventArgs e)
+        private async void mainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             Data.CreateTable();
             listViewData.ItemsSource = Data.GetAllData();
+            update =await UpdateManager.GitHubUpdateManager(@"https://github.com/dyako-baram/WpfWithAutoUpdate");
 
+
+        }
+        public async void CheckForUpdate()
+        {
+            var updateInfo = await update.CheckForUpdate();
+
+            if (updateInfo.ReleasesToApply.Count > 0)
+            {
+                var dialog = MessageBox.Show("do you want update to newer version?", "Update?", MessageBoxButton.YesNo);
+                if(dialog == MessageBoxResult.Yes)
+                {
+                    await update.UpdateApp();
+                    MessageBox.Show("Updated succesfuly!\n Please Restart to apply the changes");
+                }
+            }
+            
         }
         private void buttonAddToListView_Click(object sender, RoutedEventArgs e)
         {
