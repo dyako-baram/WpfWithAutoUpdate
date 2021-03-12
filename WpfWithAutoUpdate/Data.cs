f1c5d59862f57744c85f80f0c8974600ce998b72
Dyako Baram <55919885+dyako-baram@users.noreply.github.com>
2021-03-12 18:54:28 +0300

    Add project files.

---
+++ WpfWithAutoUpdate/Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace WpfWithAutoUpdate
+{
+    public class Data
+    {
+        public static void SaveData(string name)
+        {
+            using (var con=new SqliteConnection(GetConnection()))
+            {
+                con.Execute("insert into Names values (@Name)",new { Name=name});
+            }
+        }
+        public static List<string> GetAllData()
+        {
+            using (var con = new SqliteConnection(GetConnection()))
+                return con.Query<string>("Select Name from Names").ToList();
+        }
+        public static void CreateTable()
+        {
+            using (var con = new SqliteConnection(GetConnection()))
+            {
+                con.Execute("create table if not exists Names(Name TEXT)");
+            }
+        }
+        public static string GetConnection()
+        {
+            return "DataSource=DataBase.db";
+        }
+    }
+}
---
+++ WpfWithAutoUpdate/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWithAutoUpdate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void buttonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void mainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Data.CreateTable();
+            listViewData.ItemsSource = Data.GetAllData();
+
+        }
+        private void buttonAddToListView_Click(object sender, RoutedEventArgs e)
+        {
+            Data.SaveData(textName.Text);
+            listViewData.ItemsSource = Data.GetAllData();
+
+        }
+    }
+}
